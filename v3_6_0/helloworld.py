1fca7f192293c0071f29a1c905b48ef46b1f2ef5
Makoto Morita <mmakmo@gmail.com>
2017-02-22 23:20:10 +0900

    update sample

--- v3_6_0/helloworld.py
+++ v3_6_0/helloworld.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 
+import sys
+
 # -*- coding: utf-8 -*-
 
-print("Hello World")
+
+def greet(name):
+    print('Hello, {0}!'.format(name))
+
+
+if len(sys.argv) > 1:
+    name = sys.argv[1]
+    greet(name)
+else:
+    greet('world')
